06aed3eec7d80595c0dca2eae01e06fb4598a0b1
zmz <3091279505@qq.com>
2023-08-28 10:34:59 +0800

    添加了一些.java文件

--- Ajax05_demo1/src/main/java/com/example/web/SelectAllServlet.java
+++ Ajax05_demo1/src/main/java/com/example/web/SelectAllServlet.java
@@ -1,6 +1,8 @@
 package com.example.web;
 
 
+import com.alibaba.fastjson.JSON;
+import com.example.pojo.Brand;
 import com.example.service.BrandService;
 
 import javax.servlet.ServletException;
@@ -9,6 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/select-all")
 public class SelectAllServlet extends HttpServlet {
@@ -17,18 +20,16 @@ public class SelectAllServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 1.调用service查询
-//        branService.selectAll()
+        List<Brand> brands = branService.selectAll();
         // 2.将集合转换为JSON数据，序列化
-
-
-        // 3.响应数据
-
-
-
+        String jsonString = JSON.toJSONString(brands);
+        // 3.返回响应数据
+        resp.setContentType("text/json;charset=utf-8");
+        resp.getWriter().write(jsonString);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        this.doGet(req, resp);
     }
 }
